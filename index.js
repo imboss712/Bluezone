dc4120a1997c64466f2341418444516ffe393035
Shashi Kant Yadav <shashikant231217@gmail.com>
2021-08-25 09:44:49 +0530

    csp disabled

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json({ limit: '20kb' }));
 app.use(express.urlencoded({ extended: false, limit: '20kb' }));
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false
+  })
+);
 app.use(cors());
 app.use(hpp());
 app.use(xss());
